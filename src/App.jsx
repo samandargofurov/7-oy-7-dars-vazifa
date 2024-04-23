30bc2538c1a439c6c44288cc4491686a99af87aa
samandargofurov <samandargofurov236@gmail.com>
2024-04-23 23:03:31 +0500

    Routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/about' element={<Home></Home>}></Route>
+      <Route path='/products' element={<Home></Home>}></Route>
+      <Route path='/cart' element={<Home></Home>}></Route>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/' element={<Home></Home>}></Route>
+    </Routes>
   )
 }
 
