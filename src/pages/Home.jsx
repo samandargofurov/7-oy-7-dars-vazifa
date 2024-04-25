93b835d36ff5d31848aa3cb9873290cc36ffa906
samandargofurov <samandargofurov236@gmail.com>
2024-04-25 21:58:03 +0500

    8 lesson

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,6 @@ function Home() {
       <div className='container w-3/4 mx-auto mt-20 mb-20'>
         <h2 className='font-bold text-3xl text-[#394E6A] mb-7'>Featured Products</h2>
         <hr />
-        
 
         <div className='flex justify-between gap-5'>
           <Card></Card>
